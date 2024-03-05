b6cc4442addafd74f8008aa2b13430c20791f598
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2024-03-04 20:58:47 -0500

    initial commit

---
+++ Arrays.js
@@ -0,0 +1,12 @@
+const items = ["apples", "bananas", "grapses", "mushrooms"];
+
+//measuring the number of steps is the key to analyze the speed of an operation
+//time complexity, performance, efficient, speed
+
+
+console.log(items[5]?.length)
+
+
+//reading a values at a certain index is a one step process, fastest
+//Time Comple: O (1)
+//Linear Search: O (n)
